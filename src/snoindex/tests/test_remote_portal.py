ff0a64a2fe82ae65c33ce6eb8b87a3a481ab67d2
Keenan Graham <ksgraham@stanford.edu>
2022-10-06 15:19:30 -0700

    Add tests

---
+++ src/snoindex/tests/test_remote_portal.py
@@ -0,0 +1,37 @@
+import pytest
+
+
+def test_remote_portal_make_remote_request():
+    assert False
+
+
+def test_remote_portal_make_authorized_remote_request():
+    assert False
+
+
+def test_remote_portal_portal_init():
+    assert False
+
+
+def test_remote_portal_portal_make_index_data_view_url_from_uuid():
+    assert False
+
+
+def test_remote_portal_portal_get_raw_item_by_uuid():
+    assert False
+
+
+def test_remote_portal_portal_get_item():
+    assert False
+
+
+def test_remote_portal_wait_for_portal_connection():
+    assert False
+
+
+def test_remote_portal_portal_make_access_key_url():
+    assert False
+
+
+def test_remote_portal_portal_wait_for_access_key_to_exist():
+    assert False
---
+++ src/snoindex/tests/test_repository_queue_sqs.py
@@ -0,0 +1,50 @@
+import pytest
+
+
+def test_repository_queue_sqs_batch():
+    assert False
+
+
+def test_repository_queue_sqs_sqsqueue_init():
+    assert False
+
+
+def test_repository_queue_sqs_private_send_messages():
+    assert False
+
+
+def test_repository_queue_sqs_send_messages():
+    assert False
+
+
+def test_repository_queue_sqs_private_get_messages():
+    assert False
+
+
+def test_repository_queue_sqs_get_messages():
+    assert False
+
+
+def test_repository_queue_sqs_private__mark_as_processed():
+    assert False
+
+
+def test_repository_queue_sqs_mark_as_processed():
+    assert False
+
+
+def test_repository_queue_sqs_info():
+    assert False
+
+
+def test_repository_queue_sqs_wait_for_queue_to_exist():
+    assert False
+
+
+def test_repository_queue_sqs_queue_has_zero_messages():
+    assert False
+
+
+def test_repository_queue_sqs_wait_for_queue_to_drain():
+    assert False
+ 
