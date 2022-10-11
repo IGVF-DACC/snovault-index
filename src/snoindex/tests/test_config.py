5c563ff77be83e89eb5336b8af7749656e391f36
Keenan Graham <ksgraham@stanford.edu>
2022-10-11 15:01:22 -0700

    Ignore commands

--- src/snoindex/tests/test_config.py
+++ src/snoindex/tests/test_config.py
@@ -1,15 +1,37 @@
 import pytest
 
 
-def test_get_sqs_client():
+def test_config_get_sqs_client():
     from botocore.client import BaseClient
     from snoindex.config import get_sqs_client
     client = get_sqs_client('http://localstackendpoint:4566')
     assert isinstance(client, BaseClient)
 
 
-def test_get_opensearch_client():
+def test_config_get_opensearch_client():
     from opensearchpy import OpenSearch
     from snoindex.config import get_opensearch_client
     client = get_opensearch_client('http://opensearch')
     assert isinstance(client, OpenSearch)
+
+
+@pytest.mark.integration
+def test_config_invalidation_service_config():
+    from snoindex.config import InvalidationServiceConfig
+    assert False
+
+
+@pytest.mark.integration
+def test_config_indexing_service_config(invalidation_queue, opensearch_client):
+    from snoindex.config import IndexingServiceConfig
+    from snoindex.config import get_sqs_client
+    config = IndexingServiceConfig(
+        backend_url='some-url',
+        auth=('some', 'auth'),
+        invalidation_queue=invalidation_queue,
+        opensearch_client=opensearch_client,
+        opensearch_resources_index='some-index',
+        sqs_client=get_sqs_client('http://localstackendpoint:4566')
+    )
+    assert config.backend_url == 'some-url'
+    assert isinstance(config, IndexingServiceConfig)
